f840454763ea774b4286b3bdda7981d85c05359e
cemalokten <cemalokten@gmail.com>
2021-10-26 20:17:20 +0100

    Added Prettier
    Prettier now working with Eslint (See gist https://gist.github.com/cemalokten/7ab270f26384cd3e3a306cd60e44303b)
    
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,21 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count1) => count1 + 1)}>
-            count is:
-            {' '}
-            {count}
+          <button
+            type="button"
+            onClick={() => setCount((count1) => count1 + 1)}>
+            count is: {count}
           </button>
         </p>
         <p>
-          Edit
-          {' '}
-          <code>App.jsx</code>
-          {' '}
-          and save to test HMR updates.
+          Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
@@ -38,8 +33,7 @@ function App() {
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Vite Docs
           </a>
         </p>
