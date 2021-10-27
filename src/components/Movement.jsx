780f76659dc431e537491466eb6a1465482280d7
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-27 12:05:32 +0100

    goblin middle of page
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- src/components/Movement.jsx
+++ src/components/Movement.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/Player.css';
-import Goblin from '../assets/Goblin.png';
+import goblin from '../assets/goblin.png';
 
 function Movement() {
-  return <img src={Goblin} alt="The player avatar" className="player" />;
+  return <img src={goblin} alt="The player avatar" className="player" />;
 }
 
 export default Movement;
