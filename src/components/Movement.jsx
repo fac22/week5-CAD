1b0ef3de20dbbad6d719ea6433d8931b4bf6492f
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-27 14:11:40 +0100

    part way through getting the damn goblin to move
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- src/components/Movement.jsx
+++ src/components/Movement.jsx
@@ -3,7 +3,44 @@ import '../styles/Player.css';
 import goblin from '../assets/goblin.png';
 
 function Movement() {
-  return <img src={goblin} alt="The player avatar" className="player" />;
+  const [positionX, setPositionX] = React.useState(50);
+  const [positionY, setPositionY] = React.useState(50);
+
+  function handleKeyDown(e) {
+    console.log(e.keyCode, 'KEY HAS BEEN PRESSED');
+
+    if (e.keyCode === 40) {
+      setPositionY(positionY + 1);
+      console.log('Key Down Pressed 👍 Inside useEffect');
+    }
+
+    if (e.keyCode === 38) {
+      setPositionY(positionY - 1);
+      console.log('Key Down Pressed 👍 Inside useEffect');
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const goblinPosition = {};
+
+  React.useEffect(() => {
+    goblinPosition.top = `${positionY}vh`;
+    goblinPosition.left = `${positionX}vh`;
+  }, [positionX, positionY]);
+
+  console.log('outside effect hook', positionY);
+
+  return (
+    <img
+      src={goblin}
+      style={goblinPosition}
+      alt="The player avatar"
+      className="player"
+    />
+  );
 }
 
 export default Movement;
