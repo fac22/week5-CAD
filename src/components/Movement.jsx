584474632064411b0d883eaf516a17f1efff22bd
cemalokten <cemalokten@gmail.com>
2021-10-27 16:24:23 +0100

    Game boundry inplace
    - Logic on each direction to prevent goblin from going off page
    - No more border around goblin
    
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- src/components/Movement.jsx
+++ src/components/Movement.jsx
@@ -2,14 +2,39 @@ import { useState, useEffect } from 'react';
 import Goblin from './Goblin';
 
 function Movement() {
-  const [y, setY] = useState(0);
-  const [x, setX] = useState(0);
+  const [y, setY] = useState(4);
+  const [x, setX] = useState(4);
 
   function handleKeyDown(e) {
-    if (e.keyCode === 40) setY((p) => p + 2);
-    if (e.keyCode === 38) setY((p) => p - 2);
-    if (e.keyCode === 37) setX((p) => p - 2);
-    if (e.keyCode === 39) setX((p) => p + 2);
+    if (e.keyCode === 40)
+      setY((p) => {
+        if (p + 2 !== 90) {
+          return p + 2;
+        }
+        return p;
+      });
+
+    if (e.keyCode === 38)
+      setY((p) => {
+        if (p - 2 !== -2) {
+          return p - 2;
+        }
+        return p;
+      });
+    if (e.keyCode === 37)
+      setX((p) => {
+        if (p - 2 !== -2) {
+          return p - 2;
+        }
+        return p;
+      });
+    if (e.keyCode === 39)
+      setX((p) => {
+        if (p + 2 !== 90) {
+          return p + 2;
+        }
+        return p;
+      });
   }
 
   useEffect(() => {
@@ -18,7 +43,7 @@ function Movement() {
 
   return (
     <div>
-      <Goblin y={y} x={x} />;
+      <Goblin y={y} x={x} />
     </div>
   );
 }
