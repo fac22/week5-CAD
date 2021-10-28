d452a236a743cce640c106638680f2fa179fd889
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-28 11:27:04 +0100

    removed portal size increase, shrunk goblin and increased size of portal

--- src/components/Movement.jsx
+++ src/components/Movement.jsx
@@ -11,8 +11,6 @@ function Movement() {
   const [gx, setGx] = React.useState(4);
   const [by, setBy] = React.useState(randomNumber(0, 88));
   const [bx, setBx] = React.useState(randomNumber(50, 88));
-  const [height, setHeight] = React.useState(12);
-  const [width, setWidth] = React.useState(12);
 
   function handleKeyDown(e) {
     if (e.keyCode === 38) setGy((y) => (y - 2 !== -2 ? y - 2 : y)); // up
@@ -27,8 +25,8 @@ function Movement() {
 
   const blobAxes = [setBy, setBx];
   const moveSign = [
-    (p) => (p + 2 <= 100 - width ? p + 2 : p),
-    (p) => (p - 2 >= 0 ? p - 2 : p),
+    (p) => (p + 2 <= 100 - 20 ? p + 1 : p),
+    (p) => (p - 2 >= 0 ? p - 1 : p),
   ];
 
   function blobMovement() {
@@ -37,23 +35,14 @@ function Movement() {
     return axis(sign);
   }
 
-  function blobSize() {
-    setHeight((h) => (h < 50 ? h + 0.001 : h));
-    setWidth((w) => (w < 50 ? w + 0.001 : w));
-  }
-
   React.useEffect(() => {
-    setInterval(blobSize, 1000);
-  });
-
-  React.useEffect(() => {
-    setInterval(blobMovement, 10);
+    setInterval(blobMovement, 1);
   }, []);
 
   return (
     <div>
       <Goblin y={gy} x={gx} />
-      <Blob y={by} x={bx} height={height} width={width} />
+      <Blob y={by} x={bx} />
     </div>
   );
 }
