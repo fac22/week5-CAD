de431c2fc66698bc2273d73793542ebc64a6532b
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-28 17:02:13 +0100

    high scores!
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 function Table(prop) {
   const { setStage } = prop;
   const updateStage = () => setStage('Game');
+  const { ...scores } = localStorage;
+  const scoresArr = Object.values(scores);
+  console.log(scoresArr);
 
   return (
     <div>
@@ -20,6 +23,14 @@ function Table(prop) {
         <button onClick={updateStage} type="button">
           Start
         </button>
+        <h2>Here are your previous scores!</h2>
+        <ol>
+          {scoresArr
+            .sort((a, b) => b - a)
+            .map((score, index) => (
+              <li key={index}>{score}</li>
+            ))}
+        </ol>
       </main>
     </div>
   );
