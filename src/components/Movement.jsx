e1aa74ace8db2037bb52c9a067d193fbde50c96e
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-28 12:43:48 +0100

    collision detection on goblin centre and portal inner box

--- src/components/Movement.jsx
+++ src/components/Movement.jsx
@@ -14,7 +14,24 @@ function Movement() {
   const [collision, setCollision] = React.useState(false);
 
   React.useEffect(() => {
-    if (gy === by && gx === bx) setCollision(true);
+    const goblinCentrePoint = { x: gx + 10 / 2, y: gy + 10 / 2 };
+    const portalBox = {
+      // topLeft: {x: bx + 5, y: by + 5},
+      // topRight: {x: bx + 15, y: by + 5},
+      // bottomLeft: {x: bx + 5, y: by + 15},
+      // bottomRight: {x: bx + 15, y: by + 15},
+      yMin: by + 5,
+      yMax: by + 15,
+      xMin: bx + 5,
+      xMax: bx + 15,
+    };
+    if (
+      goblinCentrePoint.y <= portalBox.yMax &&
+      goblinCentrePoint.y >= portalBox.yMin &&
+      goblinCentrePoint.x <= portalBox.xMax &&
+      goblinCentrePoint.x >= portalBox.xMin
+    )
+      setCollision(true);
   }, [gy, gx, by, bx]);
 
   React.useEffect(() => {
@@ -25,7 +42,7 @@ function Movement() {
     if (e.keyCode === 38) setGy((y) => (y - 2 !== -2 ? y - 2 : y)); // up
     if (e.keyCode === 40) setGy((y) => (y + 2 !== 92 ? y + 2 : y)); // down
     if (e.keyCode === 37) setGx((x) => (x - 2 !== -2 ? x - 2 : x)); // left
-    if (e.keyCode === 39) setGx((x) => (x + 2 !== 92 ? x + 2 : x)); // right
+    if (e.keyCode === 39) setGx((x) => (x + 2 !== 92 ? x + 2 : x)); // left
   }
 
   React.useEffect(() => {
